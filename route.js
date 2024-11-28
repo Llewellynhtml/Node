c565374e7eb62eb0291a10ae71583e2addb38d74
Llewellyn <Llewellyn.ml.info@gmail.com>
2024-11-28 16:17:40 +0200

    Shoppinglist

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const url = require('url');
 module.exports = http.createServer((req, res) => {
     var userOps = require('./controller.js');
     const reqUrl =  url.parse(req.url, true);
-// GET endpoint
+
   if(reqUrl.pathname == '/users' && req.method === 'GET') {
     console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
     userOps.getUsers(req, res);
--- userData.js
+++ userData.js
@@ -1,18 +1,14 @@
 module.exports = [
-    {
-      id: 1,
-      name: 'Seyi',
-      location: 'Buenos Aires'
-    },
-    {
-      id: 2,
-      name: 'David Khan',
-      location: 'Argentina'
-    },
-    {
-      id: 3,
-      name: 'Isaac',
-      location: 'Kuala Lumpur'
-    }
-  ];
-  
+  {
+    name: 'Milk',
+    quantity: 2
+  },
+  {
+    name: 'Bread',
+    quantity: 5
+  },
+  {
+    name: 'Eggs',
+    quantity: 12
+  }
+];
